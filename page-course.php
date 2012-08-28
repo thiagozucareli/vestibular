553231fbf7958e6ebfe19baddd044e7bc0e748ed
Thiago Zucareli <thiagozp@gmail.com>
2012-08-28 17:23:32 -0300

    floating pages

--- page-course.php
+++ page-course.php
@@ -5,7 +5,7 @@ Template Name: Course page
 ?>
 
 <?php get_header(); ?>
-<div id="scroll">
+<div id="scroll" class="clear-left">
 	<div class="course center clear">	
 		<section class="page-top center clear">
 			<div class="photo">
@@ -55,5 +55,57 @@ Template Name: Course page
 		<div class="clear"></div>
 
 	</div>
+
+	<div class="course center clear">	
+		<section class="page-top center clear">
+			<div class="photo">
+			</div>
+			<div class="video">
+				<iframe width="640" height="360" src="http://www.youtube.com/embed/C5mnU2JelWs?rel=0" frameborder="0" allowfullscreen></iframe>
+			</div>
+			<div class="information clear">
+				<section>
+					<div class="duration">
+						<p>4 anos</p>
+					</div>
+					<div class="space">
+						<p>40 vagas</p>
+					</div>
+					<div class="ordinance">
+						<p>Reconhecimento Port. Min. 1.843/1994</p>
+					</div>
+				</section>
+				<a href="#">Mais informa&ccedil;&otilde;es do curso</a>
+				<div class="clear"></div>
+			</div>
+			<div class="clear"></div>
+		</section>
+		<section class="page-hide clear">
+			<div class="occupation red">
+
+			</div>
+			<div class="opportunities blue">
+
+			</div>
+			<div class="course-in red">
+
+			</div>
+			<div class="labs blue">
+
+			</div>
+			<div class="projects red">
+
+			</div>
+			<div class="events blue">
+
+			</div>
+			<div class="clear"></div>
+		</section>
+
+		<div class="clear"></div>
+
+	</div>
+
+	<div class="clear-left"></div>
 </div>
 <?php get_footer(); ?>
