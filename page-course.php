3ac22880bbb5a72ec3fb8fbafcde8d2ebc44855d
Thiago Zucareli <thiagozp@gmail.com>
2012-08-28 21:20:06 -0300

    fixing up the clear problem

--- page-course.php
+++ page-course.php
@@ -5,15 +5,63 @@ Template Name: Course page
 ?>
 
 <?php get_header(); ?>
-<div id="scroll" class="clear">
-	<div class="course center clear">	
-		<section class="page-top center clear">
+<div id="scroll">
+	<div class="course center">	
+		<section class="page-top center">
 			<div class="photo">
 			</div>
 			<div class="video">
 				<iframe width="640" height="360" src="http://www.youtube.com/embed/C5mnU2JelWs?rel=0" frameborder="0" allowfullscreen></iframe>
 			</div>
-			<div class="information clear">
+			<div class="information">
+				<section>
+					<div class="duration">
+						<p>4 anos</p>
+					</div>
+					<div class="space">
+						<p>40 vagas</p>
+					</div>
+					<div class="ordinance">
+						<p>Reconhecimento Port. Min. 1.843/1994</p>
+					</div>
+					<div class="clear"></div>
+				</section>
+				<a href="#">Mais informa&ccedil;&otilde;es do curso</a>
+				<div class="clear"></div>
+			</div>
+			<div class="clear"></div>
+		</section>
+		<section class="page-hide">
+			<div class="occupation">
+
+			</div>
+			<div class="opportunities">
+
+			</div>
+			<div class="course-in">
+
+			</div>
+			<div class="labs">
+
+			</div>
+			<div class="projects">
+
+			</div>
+			<div class="events">
+
+			</div>
+			<div class="clear"></div>
+		</section>
+	</div>
+
+	<div class="course center">	
+		<section class="page-top center">
+			<div class="photo">
+			</div>
+			<div class="video">
+				<iframe width="640" height="360" src="http://www.youtube.com/embed/C5mnU2JelWs?rel=0" frameborder="0" allowfullscreen></iframe>
+			</div>
+			<div class="information">
 				<section>
 					<div class="duration">
 						<p>4 anos</p>
@@ -26,86 +74,28 @@ Template Name: Course page
 					</div>
 				</section>
 				<a href="#">Mais informa&ccedil;&otilde;es do curso</a>
-				<div class="clear"></div>
 			</div>
-			<div class="clear"></div>
 		</section>
-		<section class="page-hide clear">
-			<div class="occupation red">
+		<section class="page-hide">
+			<div class="occupation">
 
 			</div>
-			<div class="opportunities blue">
+			<div class="opportunities">
 
 			</div>
-			<div class="course-in red">
+			<div class="course-in">
 
 			</div>
-			<div class="labs blue">
+			<div class="labs">
 
 			</div>
-			<div class="projects red">
+			<div class="projects">
 
 			</div>
-			<div class="events blue">
+			<div class="events">
 
 			</div>
-			<div class="clear"></div>
 		</section>
-
-		<div class="clear"></div>
-
 	</div>
-
-	<div class="course center clear">	
-		<section class="page-top center clear">
-			<div class="photo">
-			</div>
-			<div class="video">
-				<iframe width="640" height="360" src="http://www.youtube.com/embed/C5mnU2JelWs?rel=0" frameborder="0" allowfullscreen></iframe>
-			</div>
-			<div class="information clear">
-				<section>
-					<div class="duration">
-						<p>4 anos</p>
-					</div>
-					<div class="space">
-						<p>40 vagas</p>
-					</div>
-					<div class="ordinance">
-						<p>Reconhecimento Port. Min. 1.843/1994</p>
-					</div>
-				</section>
-				<a href="#">Mais informa&ccedil;&otilde;es do curso</a>
-				<div class="clear"></div>
-			</div>
-			<div class="clear"></div>
-		</section>
-		<section class="page-hide clear">
-			<div class="occupation red">
-
-			</div>
-			<div class="opportunities blue">
-
-			</div>
-			<div class="course-in red">
-
-			</div>
-			<div class="labs blue">
-
-			</div>
-			<div class="projects red">
-
-			</div>
-			<div class="events blue">
-
-			</div>
-			<div class="clear"></div>
-		</section>
-
-		<div class="clear"></div>
-
-	</div>
-
-	<div class="clear-left"></div>
 </div>
 <?php get_footer(); ?>
