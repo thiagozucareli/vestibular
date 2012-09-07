6002cbc238fee894bf9501994192e71710a91630
Thiago Zucareli <thiagozp@gmail.com>
2012-09-07 20:16:52 -0300

    Adding menu on idex.php

--- index.php
+++ index.php
@@ -7,8 +7,48 @@
 
 <?php get_header(); ?>
 	
-	<div id="menu">
-
+	<div id="menu-courses">
+		<ul>
+			<li>
+				<a href="#">Administração</a>
+			</li>
+			<li>
+				<a href="#">Arquitetura</a>
+			</li>
+			<li>
+				<a href="#">Biologia</a>
+			</li>
+			<li>
+				<a href="#">Biblioteconomia</a>
+			</li>
+			<li>
+				<a href="#">Computação</a>
+			</li>
+			<li>
+				<a href="#">Design</a>
+			</li>
+			<li>
+				<a href="#">Educação Artistica</a>
+			</li>
+			<li>
+				<a href="#">Enfermagem</a>
+			</li>
+			<li>
+				<a href="#">Jornalismo</a>
+			</li>
+			<li>
+				<a href="#">Letras</a>
+			</li>
+			<li>
+				<a href="#">Pedagogia</a>
+			</li>
+			<li>
+				<a href="#">Pub. e Propaganda</a>
+			</li>
+			<li>
+				<a href="#">Radio e Tv</a>
+			</li>
+		</ul>
 	</div>
 
 <?php get_footer(); ?>
