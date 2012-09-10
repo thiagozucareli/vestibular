11ffc59bd9ec05d9d88895016ff7ae05bd534584
Matheus E. Muller <hello@memuller.com>
2012-09-10 16:05:26 -0300

    Properly sorts courses in the menu.

--- menu-course.php
+++ menu-course.php
@@ -1,6 +1,6 @@
 <div id="menu-courses">
 	<ul>
-		<?php $courses = get_posts(array('post_type' => 'course', 'orderby' => 'post_title', 'order' => 'asc')) ?>
+		<?php $courses = get_posts(array('post_type' => 'course', 'orderby' => 'title', 'order' => 'ASC')) ?>
 		<?php foreach ($courses as $course): ?>
 			<li>
 				<a href="<?php echo "cursos/#$course->post_name" ?>"><?php echo $course->post_title ?></a>
