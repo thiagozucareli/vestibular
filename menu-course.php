9da65f1248090596ae4bae64a4482b37e76ff142
Matheus E. Muller <hello@memuller.com>
2012-09-10 16:39:53 -0300

    Shows more courses.

--- menu-course.php
+++ menu-course.php
@@ -1,6 +1,6 @@
 <div id="menu-courses">
 	<ul>
-		<?php $courses = get_posts(array('post_type' => 'course', 'orderby' => 'title', 'order' => 'ASC')) ?>
+		<?php $courses = get_posts(array('post_type' => 'course', 'orderby' => 'title', 'order' => 'ASC', 'numberposts' => 13)) ?>
 		<?php foreach ($courses as $course): ?>
 			<li>
 				<a href="<?php echo "cursos/#$course->post_name" ?>"><?php echo $course->post_title ?></a>
