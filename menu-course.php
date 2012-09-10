bd2819bcf9f2a38baa5961f5e86b0a3327823f0f
Matheus E. Muller <hello@memuller.com>
2012-09-10 11:39:38 -0300

    Fixes syntar error on order by clausule.

--- menu-course.php
+++ menu-course.php
@@ -1,6 +1,6 @@
 <div id="menu-courses">
 	<ul>
-		<?php $courses = get_posts(array('post_type' => 'course', 'order_by' => 'post_title', 'order' => 'asc')) ?>
+		<?php $courses = get_posts(array('post_type' => 'course', 'orderby' => 'post_title', 'order' => 'asc')) ?>
 		<?php foreach ($courses as $course): ?>
 			<li>
 				<a href="<?php echo "cursos/#$course->post_name" ?>"><?php echo $course->post_title ?></a>
