52182fa8a104269462dbc6344177f9720ab91892
Matheus E. Muller <hello@memuller.com>
2012-09-10 11:28:28 -0300

    Dynamizes course list on home page.

--- index.php
+++ index.php
@@ -7,49 +7,7 @@
 
 <?php get_header(); ?>
 	
-	<div id="menu-courses">
-		<ul>
-			<li>
-				<a href="#">Administração</a>
-			</li>
-			<li>
-				<a href="#">Arquitetura</a>
-			</li>
-			<li>
-				<a href="#">Biologia</a>
-			</li>
-			<li>
-				<a href="#">Biblioteconomia</a>
-			</li>
-			<li>
-				<a href="#">Computação</a>
-			</li>
-			<li>
-				<a href="#">Design</a>
-			</li>
-			<li>
-				<a href="#">Educação Artistica</a>
-			</li>
-			<li>
-				<a href="#">Enfermagem</a>
-			</li>
-			<li>
-				<a href="#">Jornalismo</a>
-			</li>
-			<li>
-				<a href="#">Letras</a>
-			</li>
-			<li>
-				<a href="#">Pedagogia</a>
-			</li>
-			<li>
-				<a href="#">Pub. e Propaganda</a>
-			</li>
-			<li>
-				<a href="#">Radio e Tv</a>
-			</li>
-		</ul>
-	</div>
+	<?php get_template_part('menu', 'course'); ?>
 
 	<section id="content-home">
 			<div class="photo">
---
+++ menu-course.php
@@ -0,0 +1,10 @@
+<div id="menu-courses">
+	<ul>
+		<?php $courses = get_posts(array('post_type' => 'course', 'order_by' => 'post_title', 'order' => 'asc')) ?>
+		<?php foreach ($courses as $course): ?>
+			<li>
+				<a href="<?php echo "cursos/#$course->post_name" ?>"><?php echo $course->post_title ?></a>
+			</li>
+		<?php endforeach ?>
+	</ul>
+</div>
