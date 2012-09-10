5a54d3087deea11ba98149e3aacd682f38770bc5
Matheus E. Muller <hello@memuller.com>
2012-09-10 02:47:40 -0300

    Handles 'set' fields on metadata accessors.

--- functions.php
+++ functions.php
@@ -1,13 +1,18 @@
 <?php 
 	function get_the($field){
 		global $post, $post_object ;
+		
 		if($post->post_type == 'page') 
 			return get_post_meta($post->ID, $field, true); 
+
+		$class = '\\FateaVestibular\\' . ucfirst($post->post_type) ;
+		
 		if (empty($post_object) || $post_object->ID != $post->ID){
-			$post_type = $post->post_type ; 
-			$class = '\\FateaVestibular\\' . ucfirst($post_type) ;
 			$post_object = new $class($post) ;
 		}
+		
+		if($class::$fields[$field]['type'] == 'set') return $post_object->set($field);
+		
 		return $post_object->$field ;
 
 	}
