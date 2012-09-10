8fa5f8bfdecc8d667dce86659dde4b7a7a1a6f0d
Matheus E. Muller <hello@memuller.com>
2012-09-10 05:06:49 -0300

    Adds page sections.

---
+++ boxes-page.php
@@ -0,0 +1,9 @@
+<section id="pages" style='display:none;'>
+	<?php $pages = get_pages(); ?>
+	<?php foreach ($pages as $page): ?>
+		<div class="page" id="<?php echo $page->post_name ?>">
+			<h2><?php echo $page->post_title ?></h2>
+			<?php echo $page->post_content ?>
+		</div>
+	<?php endforeach ?>
+</section>
--- footer.php
+++ footer.php
@@ -1,3 +1,4 @@
+		<?php get_template_part('boxes', 'page') ?>
 		<section id="footer">
 			<div id="menu">
 				<?php wp_nav_menu(); ?>
